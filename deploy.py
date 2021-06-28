f3b72c9e3f73493a5ed430784528301055230957
Min RK <benjaminrk@gmail.com>
2021-06-28 11:52:02 +0200

    fix text label for only-one hub selected
    text wants to be a string, not a list

--- deploy.py
+++ deploy.py
@@ -141,7 +141,7 @@ def populate_template_variables(api, db):
             var["current"] = {
                 "selected": True,
                 "tags": [],
-                "text": labels[:1],
+                "text": labels[0],
                 "value": labels[:1],
             }
             var["options"][0]["selected"] = True
